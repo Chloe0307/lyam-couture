a825d739d0ee3079eea4e2b0cddb885d9eb27e64
chloe0307 <chloecuny@yahoo.fr>
2020-08-22 10:55:09 +0200

    add picture header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable max-len */
 import React from 'react';
 
 import Nav from 'src/components/Header/Nav/Navbar';
+
+// == import styles and pictures
+import Picture from 'src/assets/pictures/headerPicture.jpg';
 import './header.scss';
 
 const Header = () => (
@@ -9,6 +13,7 @@ const Header = () => (
     <div className="header-title-content">
       <h1 className="header-bigTitle"><strong>Lya'm</strong></h1>
       <h2 className="header-title">Couture...</h2>
+      <img src={Picture} alt="photo de présentation de produits" className="header-picture" />
     </div>
     <Nav />
   </header>
