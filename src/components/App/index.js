743e72a66e4046900ca48ab150cec96890ebc58d
chloe0307 <chloecuny@yahoo.fr>
2020-09-05 10:12:18 +0200

    make footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,7 @@ import Header from 'src/components/Header';
 import Home from 'src/components/Home';
 import Footer from 'src/components/Footer';
 
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 import './footer.scss';
 
 const Footer = () => (
-  <footer>
-    Je suis le footer
+  <footer className="footer-content">
+    <nav className="footer-nav">
+      <li>Contacts</li>
+      <li>Mentions Légales</li>
+      <li>ChloeCuny-web-developper</li>
+      <p className="copyright">Lya'm Couture © 2020</p>
+    </nav>
   </footer>
 );
 
